f7327b3834bd8e85967daac8b0f3561ae9726709
Anmol Singh <anmol.singhdelhi1@gmail.com>
2026-05-28 22:56:32 +0530

    Create basicFunction.cpp

---
+++ function/basicFunction.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+void greeting(){
+    cout<<"Heyy! It's me Anmol"<<endl;
+    cout<<"Howdy"<<endl;
+}
+int main(){
+    greeting(); //fn calling
+    cout<<sqrt(9);  //inbuilt fn
+
+}
