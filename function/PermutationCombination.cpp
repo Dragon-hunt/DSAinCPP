572480dfe0537c7a1d23f8dc5a9106134f242d5c
Anmol Singh <anmol.singhdelhi1@gmail.com>
2026-05-29 20:01:33 +0530

    Add files via upload

---
+++ function/PermutationCombination.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int fact(int x){
+    int f=1;
+    for(int i=2; i<=x; i++){
+        f*=i;
+
+    }
+    return f;
+}
+
+int combination(int n, int r){
+    int c= fact(n)/(fact(r) *fact(n-r));
+    return c;
+}
+
+int permutation(int n, int r){
+    int p= fact(n)/fact(n-r);
+
+    return p;
+}
+
+int main(){
+    int n,r;
+    
+    cin>>n>>r;
+
+    cout<<"nCr is: "<<combination(n,r)<<endl;
+    cout<<"nPr is: "<<permutation(n,r);
+}
