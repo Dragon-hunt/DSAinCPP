e151dcf2a444465be4eef832ccb5dfaf5fef7bd3
Anmol Singh <anmol.singhdelhi1@gmail.com>
2026-05-29 20:02:50 +0530

    Update PermutationCombination.cpp

--- function/PermutationCombination.cpp
+++ function/PermutationCombination.cpp
@@ -5,7 +5,6 @@ int fact(int x){
     int f=1;
     for(int i=2; i<=x; i++){
         f*=i;
-
     }
     return f;
 }
@@ -17,13 +16,11 @@ int combination(int n, int r){
 
 int permutation(int n, int r){
     int p= fact(n)/fact(n-r);
-
     return p;
 }
 
 int main(){
     int n,r;
-    
     cin>>n>>r;
 
     cout<<"nCr is: "<<combination(n,r)<<endl;
